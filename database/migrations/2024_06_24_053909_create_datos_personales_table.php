ffc93fb79812f689b8e990364717d1fb988d66c3
marakim <paulo1rojas2@gmail.com>
2024-06-24 03:16:44 -0500

    avance migracion

---
+++ database/migrations/2024_06_24_053909_create_datos_personales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('datos_personales', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres');
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->string('dni');
+            $table->date('fecha_nacimiento');
+            $table->string('genero');
+            $table->string('nacionalidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('datos_personales');
+    }
+};
