7d0d5bae255a2fd3e498cc8e63f29414af62c4ba
luminate123 <ssenmache8@gmail.com>
2024-06-27 01:04:35 -0500

    empleos_table

---
+++ app/Models/Empleo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Empleo extends Model
+{
+    use HasFactory; 
+
+    protected $table = 'empleos';
+
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'fecha_publicacion',
+        'link',
+    ]; 
+}
---
+++ database/migrations/2024_06_27_055942_create_empleos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->text('descripcion');
+            $table->date('fecha_publicacion');
+            $table->text('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleos');
+    }
+};
